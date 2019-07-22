e69e66c0f508dcb5f633da967f8356e97a03264f
Toron Seigler <toronseigler9@gmail.com>
2019-07-22 17:35:40 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,7 @@
 #write your code here
 
 def countdown (seconds_to_midnight)
-    seconds_to_midnight = 10
   while seconds_to_midnight > 0
-    seconds_to_midnight
+     seconds_to_midnight -= 1
   end
 end
