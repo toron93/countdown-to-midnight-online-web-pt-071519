b469df06e617e7922c922ae9980cc83ad6f42bb6
Toron Seigler <toronseigler9@gmail.com>
2019-07-22 18:27:22 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
 #write your code here
 
-def countdown
+def countdown (seconds_to_midnight)
+  while seconds_to_midnight > 0
+    puts "#{seconds_to_midnight} SECOND(S)!"
+     seconds_to_midnight -= 1
+   end
+
+       "HAPPY NEW YEAR!"
 end
+  time = Time.now
+   sleep 2.seconds until Time.now > time + 10.seconds
